cb1da3f9dd88f13560264f7b4c2d5ca534d12118
jersonb <jersonlbrito@hotmail.com>
2023-06-25 21:47:46 -0300

    adjust position

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ with sync_playwright() as p:
     contents = page.locator('#contents').get_by_role('link').all()
     i = 1
     for content in contents:
+        print(f'{i}/{len(contents)}')
         video_title = content.text_content()
         video_link =  f'{root_link}{content.get_attribute("href")}' 
         video_page = browser.new_page()
         video_page.goto(video_link)
         likes_count = video_page.get_by_role("button", name=re.compile("Marque este vídeo como*")).text_content()
-        print(f'{i}/{len(contents)}')
-        i+=1
         writerow_csv([video_title, video_link, likes_count])
+        i+=1
        
     
     browser.close()

377c2b108ab2e17e50019f3f153e0d45fa3238bd
jersonb <jersonlbrito@hotmail.com>
2023-06-28 19:18:37 -0300

    feat: headless

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ https://github.com/microsoft/playwright-python
 
 with sync_playwright() as p:
     for browser_type in [p.chromium, p.firefox, p.webkit]:
-        browser = browser_type.launch()
+        browser = browser_type.launch(headless=False)
         page = browser.new_page()
         page.goto('http://playwright.dev')
         page.screenshot(path=f'example-{browser_type.name}.png')
